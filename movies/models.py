2044b211f6fc0edffcc6ef1a06afb01dfc1a3620
sashav26 <145812003+sashav26@users.noreply.github.com>
2026-09-14 00:16:26 -0400

    bugfix reports

--- movies/models.py
+++ movies/models.py
@@ -23,7 +23,6 @@ class Report(models.Model):
     id = models.AutoField(primary_key=True)
     date = models.DateTimeField(auto_now_add=True)
     review = models.ForeignKey(Review, on_delete = models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
         return str(self.id) + ' - ' +  self.review.comment
 
